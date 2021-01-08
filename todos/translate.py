c362ecdec2865dff048febdacac5c31bf6cd425a
EC2 Default User <ec2-user@ip-172-31-65-188.ec2.internal>
2021-01-08 19:22:27 +0000

    traduccion 6

--- todos/translate.py
+++ todos/translate.py
@@ -21,20 +21,16 @@ def translate(event, context):
         }
     )
     
-    translateText =  translateAWS.translate_text(Text=result["Item"]["text"], SourceLanguageCode="es", TargetLanguageCode=event['pathParameters']['lang'])
+    translateText =  translateAWS.translate_text(Text=result['Item']['text'], SourceLanguageCode='auto', TargetLanguageCode=event['pathParameters']['lang'])
 
-    item = {
-        'id': result["Item"]["id"],
-        'text': translateText,
-        'checked': result["Item"]["checked"],
-        'createdAt': result["Item"]["createdAt"],
-        'updatedAt': result["Item"]["updatedAt"],
-    }
+  
+    result['Item']['text'] = translateText['Item']['TranslatedText'];
+    
 
     # create a response
     response = {
         "statusCode": 200,
-        "body": json.dumps(item,
+        "body": json.dumps(result['Item'],
                            cls=decimalencoder.DecimalEncoder)
         
     }
